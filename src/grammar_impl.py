8567588d2bf49845fb392ad41504b82b261d9955
binaryarchivist <corneliu.nastas@isa.utm.md>
2023-04-30 11:23:14 +0300

    lab4: 4/5

--- src/grammar_impl.py
+++ src/grammar_impl.py
@@ -1,49 +1,135 @@
 import random
 from typing import List, Tuple
+import pprint
 
+
+# p = {
+#     'S': ['aB', 'bA', 'B'],
+#     'A': ['b', 'aD', 'AS', 'bAB', 'ε'],
+#     'B': ['a', 'bS'],
+#     'C': ['AB'],
+#     'D': ['BB']
+# }
 
 class Grammar:
-    def __init__(self, vn: List[str], vt: List[str], p: List[Tuple[str, str, str]]) -> None:
-        self._vn: List[str] = vn
-        self._vt: List[str] = vt
-        self._p: List[Tuple[str, str, str]] = p
+    def __init__(self, vn, vt, p, start_symbol) -> None:
+        self.vn = vn
+        self.vt = vt
+        self.p = p
+        self.start_symbol = start_symbol
 
-    def get_type(self, string: str) -> str:
-        types: List[bool] = [False, False, False, False]
-        types[3] = True  # Type 3
-        # todo
+    def eliminate_epsilon(self):
+        # Step 1: Identify all non-terminal symbols that can derive epsilon
+        eps_symbols = set()
+        for symbol in self.vn:
+            if 'ε' in self.p[symbol]:
+                eps_symbols.add(symbol)
 
-        return "Type 3"
+        # Step 2: Generate new productions without eps symbols
+        new_prods = dict(self.p)
+        while True:
+            no_new_prod = True
+            for symbol, prods in self.p.items():
+                for prod in prods:
+                    for eps_symbol in eps_symbols:
+                        if eps_symbol in prod:
+                            new_prod = prod.replace(eps_symbol, 'ε')
+                            if new_prod not in new_prods[symbol]:
+                                new_prods[symbol].append(new_prod)
+                                no_new_prod = False
+            if no_new_prod:
+                break
 
+        # Step 3: Add new productions to handle eps symbol usage in other rules
+        for symbol, productions in new_prods.items():
+            new_prods[symbol] += [production for production in self.p[symbol]
+                                  if all([eps_symbol not in production for eps_symbol in eps_symbols])]
+            for eps_symbol in eps_symbols:
+                if eps_symbol in new_prods[symbol]:
+                    new_prods[symbol].remove(eps_symbol)
+                    new_prods[symbol] += [production for production in new_prods[eps_symbol]
+                                          if production not in new_prods[symbol]]
+            new_prods[symbol] = list(set(new_prods[symbol]))
 
+        # Step 4: Remove epsilon productions
+        for symbol, productions in new_prods.items():
+            if 'ε' in productions:
+                new_prods[symbol].remove('ε')
+            new_vals = []
+            for prod in productions:
+                t = prod.replace('ε', '')
+                new_vals.append(t)
+            new_prods[symbol] = new_vals
 
+        # Step 5: Remove redundant productions
+        for symbol, productions in new_prods.items():
+            new_prods[symbol] = list(set(productions))
 
-    def generate_string(self, string: str) -> str:
-        flag: bool = True
-        while flag:
-            flag = False
-            for i in range(len(string)):
-                if self.is_non_terminal(string[i]):
-                    choices: List[int] = self.get_indices(string[i])
-                    rule_index: int = random.choice(choices)
-                    r: str = self.get_rule(rule_index)
+        self.p = new_prods
 
-                    string = string[:i] + r + string[i + 1:]
-                    flag = True
-        return string[:-1]
+    def eliminate_renaming(self):
+        # Create a dictionary of all renaming productions
+        renaming_dict = {}
+        for var in self.vn:
+            renaming_dict[var] = [var]
 
-    def get_indices(self, string: str) -> List[int]:
-        result: List[int] = []
-        for tran in self._p:
-            if string == tran[0]:
-                result.append(self._p.index(tran))
-        return result
+        while True:
+            updated = False
+            for var in self.vn:
+                new_renaming = []
+                for prod in self.p[var]:
+                    if len(prod) == 1 and prod[0] in self.vn:
+                        new_renaming.extend(renaming_dict[prod[0]])
+                    else:
+                        new_renaming.append(prod)
+
+                if set(new_renaming) != set(self.p[var]):
+                    updated = True
+                    self.p[var] = new_renaming
+
+            if not updated:
+                break
+
+        # Remove all renaming productions
+        for var in self.vn:
+            new_productions = []
+            for prod in self.p[var]:
+                if len(prod) == 1 and prod[0] in self.vn:
+                    continue  # skip renaming productions
+                else:
+                    new_productions.append(prod)
+            self.p[var] = new_productions
+
+    def eliminate_inaccessible(self):
+        # Mark the start symbol as accessible
+        accessible = set([self.start_symbol])
+
+        # Find all accessible nonterminal symbols
+        while True:
+            prev_accessible = set(accessible)
+            for nt, prod in self.p.items():
+                if nt in accessible:
+                    for p in prod:
+                        for symbol in p:
+                            if symbol in self.vn:
+                                accessible.add(symbol)
+            if prev_accessible == accessible:
+                break
+
+        # Remove inaccessible productions and symbols
+        self.p = {nt: [p for p in prod if all(
+            s in accessible or s in self.vt for s in p)] for nt, prod in self.p.items() if nt in accessible}
+        self.vn = accessible
+
+    def eliminate_non_productive_rules(self):
+            return 't'
+
 
     def is_terminal(self, string: str) -> bool:
-        return string in self._vt
+        return string in self.vt
 
     def is_non_terminal(self, string: str) -> bool:
-        return string in self._vn
+        return string in self.vn
 
     def get_rule(self, index: int) -> str:
-        return self._p[index][1] + self._p[index][2]
+        return self.p[index][1] + self.p[index][2]
--- src/main.py
+++ src/main.py
@@ -1,32 +1,76 @@
-from tokenizer import Tokenizer
 import pprint
-
-class TestTokenizer:
-    def test_next_token(self) -> any:
-        input: str = """
-        place p1, p2;
-        place p3, p4;
-        tran t1, t2;
-        p1.amm = 3;
-        p1.cap = 4;
-        p2.cap = 4;
-        t1.out = {p1 : 2, p2 : 3};
-        p1.out = {t2 : 5};
-        p2.in = {t1, t2};
-        t2.in = {p3, p4};"""
-
-        tokenizer: Tokenizer = Tokenizer(input)
-        tok = tokenizer.next_token()
-        tokens = [tok]
-
-        while tok.type != 'EOF':
-            tok = tokenizer.next_token()
-            tokens.append(tok)
-        return tokens
+from grammar_impl import Grammar
 
 
-test: TestTokenizer = TestTokenizer()
+'''
+V18
+1) Eliminate epsilon productions
+2) Eliminate any renaming
+3) Eliminate inaccessible symbols
+4) Eliminate non-productive symbols
+5) Obtain Chomsky Normal Form
+---
+G = { V_n, V_t, P, S }
+V_n = { S, A, B, C, D }
+V_t = { a, b }
+P = {
+1. S -> aB
+2. S -> bA
+3. S -> B
+4. A -> b
+5. A -> aD
+6. A -> AS
+7. A -> bAB
+8. A -> ε
+9. B -> a
+10. B -> bS
+11. C -> AB
+12. D -> BB
+}
+'''
 
-tokens = test.test_next_token()
+vn = {'S', 'A', 'B', 'C', 'D'}
+vt = {'a', 'b'}
+p = {
+    'S': ['aB', 'bA', 'B'],
+    'A': ['b', 'aD', 'AS', 'bAB', 'ε'],
+    'B': ['a', 'bS'],
+    'C': ['AB'],
+    'D': ['BB']
+}
+start_symbol = 'S'
 
-pprint.pprint(tokens)
+grammar = Grammar(vn, vt, p, start_symbol)
+for k,v in grammar.p.items():
+    print(k,v)
+print('---')
+grammar.eliminate_epsilon()
+# S ['aB', 'B', 'b', 'bA']
+# A ['bAB', 'b', 'S', 'aD', 'AS', 'bB']
+# B ['bS', 'a']
+# C ['AB', 'B']
+# D ['BB']
+for k,v in grammar.p.items():
+    print(k,v)
+print('---')
+grammar.eliminate_renaming()
+# S ['bA', 'aB', 'b']
+# A ['AS', 'b', 'aD', 'bAB', 'bB']
+# B ['a', 'bS']
+# C ['AB']
+# D ['BB']
+
+for k,v in grammar.p.items():
+    print(k,v)
+print('---')
+
+# grammar.eliminate_inaccessible()
+# for k,v in grammar.p.items():
+#     print(k,v)
+# print('---')
+
+
+
+grammar.eliminate_non_productive_rules()
+print(grammar.vn)
+
